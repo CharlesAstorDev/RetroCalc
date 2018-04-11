1e96680869a68f1966732b7fd2e55f0feb29ea0e
Charles Astor <cbastor@gmail.com>
2018-04-10 22:30:03 -0500

    Commit

--- RetroCalc/ViewController.swift
+++ RetroCalc/ViewController.swift
@@ -7,19 +7,106 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var btnSound = AVAudioPlayer()
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var rightValue = ""
+    var leftValue = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func multiply(sender: AnyObject){
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func divide(sender: AnyObject){
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func subtract(sender: AnyObject){
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func add(sender: AnyObject){
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func equal(sender: AnyObject){
+        processOperation(operation: currentOperation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
     }
-
+    
+    func processOperation(operation: Operation){
+        if currentOperation != Operation.Empty {
+            if runningNumber != "" {
+                rightValue = runningNumber
+                runningNumber = ""
+            
+            if currentOperation == Operation.Multiply {
+                //Do something
+            result = "\(Double(leftValue)! * Double(rightValue)!)"
+                
+            }else if currentOperation == Operation.Divide{
+                //to do
+                result = "\(Double(leftValue)! / Double(rightValue)!)"
+            }else if currentOperation == Operation.Add{
+                result = "\(Double(leftValue)! + Double(rightValue)!)"
+            }else if currentOperation == Operation.Subtract{
+                result = "\(Double(leftValue)! - Double(rightValue)!)"
+                
+            }
+                leftValue = result
+                outputLbl.text = result
+         }
+            currentOperation = operation
+        
+        }
+        else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 
 }
 
